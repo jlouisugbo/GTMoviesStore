2e073cadef8f24f1ce159012b9eb10f756403265
salammurillo <sa.alam.murillo@gmail.com>
2025-02-03 14:29:52 -0500

    Fixed indentation that was causing error

--- GTmovies/moviepage/admin.py
+++ GTmovies/moviepage/admin.py
@@ -44,47 +44,47 @@ class MovieAdmin(admin.ModelAdmin):
         return custom_urls + urls
 
     def refresh_movies(self, request):
-    """
-    Refresh all movies in the database by fetching the latest data from the API.
-    """
-    api_key = settings.OMDB_API_KEY
-    movies = Movie.objects.all()  
+        """
+        Refresh all movies in the database by fetching the latest data from the API.
+        """
+        api_key = settings.OMDB_API_KEY
+        movies = Movie.objects.all()
 
-    added_count = 0
-    updated_count = 0
+        added_count = 0
+        updated_count = 0
 
-    for movie in movies:
-        url = f"https://www.omdbapi.com/?apikey={api_key}&i={movie.imdb_id}" 
-        response = requests.get(url)
+        for movie in movies:
+            url = f"https://www.omdbapi.com/?apikey={api_key}&i={movie.imdb_id}"
+            response = requests.get(url)
 
-        if response.status_code == 200:
-            movie_data = response.json()
+            if response.status_code == 200:
+                movie_data = response.json()
 
-            if movie_data.get('Response') == 'True':
-                movie.title = movie_data.get('Title', movie.title)
-                movie.year = movie_data.get('Year', movie.year)
-                movie.rated = movie_data.get('Rated', movie.rated)
-                movie.released = movie_data.get('Released', movie.released)
-                movie.runtime = movie_data.get('Runtime', movie.runtime)
-                movie.genre = movie_data.get('Genre', movie.genre)
-                movie.director = movie_data.get('Director', movie.director)
-                movie.writer = movie_data.get('Writer', movie.writer)
-                movie.actors = movie_data.get('Actors', movie.actors)
-                movie.plot = movie_data.get('Plot', movie.plot)
-                movie.language = movie_data.get('Language', movie.language)
-                movie.country = movie_data.get('Country', movie.country)
-                movie.awards = movie_data.get('Awards', movie.awards)
-                movie.poster = movie_data.get('Poster', movie.poster)
-                movie.imdb_rating = movie_data['imdbRating'] if movie_data['imdbRating'] != 'N/A' else None
-                
-                movie.save()
-                updated_count += 1
+                if movie_data.get('Response') == 'True':
+                    movie.title = movie_data.get('Title', movie.title)
+                    movie.year = movie_data.get('Year', movie.year)
+                    movie.rated = movie_data.get('Rated', movie.rated)
+                    movie.released = movie_data.get('Released', movie.released)
+                    movie.runtime = movie_data.get('Runtime', movie.runtime)
+                    movie.genre = movie_data.get('Genre', movie.genre)
+                    movie.director = movie_data.get('Director', movie.director)
+                    movie.writer = movie_data.get('Writer', movie.writer)
+                    movie.actors = movie_data.get('Actors', movie.actors)
+                    movie.plot = movie_data.get('Plot', movie.plot)
+                    movie.language = movie_data.get('Language', movie.language)
+                    movie.country = movie_data.get('Country', movie.country)
+                    movie.awards = movie_data.get('Awards', movie.awards)
+                    movie.poster = movie_data.get('Poster', movie.poster)
+                    movie.imdb_rating = movie_data['imdbRating'] if movie_data['imdbRating'] != 'N/A' else None
 
-    self.message_user(
-        request,
-        f"Movies refreshed successfully! {updated_count} updated.",
-        messages.SUCCESS
-    )
+                    movie.save()
+                    updated_count += 1
 
-    return redirect('..')
+        self.message_user(
+            request,
+            f"Movies refreshed successfully! {updated_count} updated.",
+            messages.SUCCESS
+        )
+
+        return redirect('..')
 
