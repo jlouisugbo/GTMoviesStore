f667c0ffeed7455169ef719eb152dd010a0245bf
jlouisugbo <jlouisugbo@gmail.com>
2025-02-17 01:19:29 -0500

    Added a frontpage scrolling movie thing

--- GTmovies/settings.py
+++ GTmovies/settings.py
@@ -126,17 +126,14 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os
 
-STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'GTmovies/static/',
-
+    BASE_DIR / 'GTmovies/static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
