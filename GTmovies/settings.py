a34e8144fe5d3c50f4efdca018d99cad07fd9da8
jlouisugbo <jlouisugbo@gmail.com>
2025-02-17 08:00:56 -0500

    Update settings.py

--- GTmovies/settings.py
+++ GTmovies/settings.py
@@ -126,9 +126,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'GTmovies' / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'  
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"] 
+STATIC_ROOT = BASE_DIR / "staticfiles"  
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 
