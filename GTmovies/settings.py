d29a5f3aa136b59cceb45068079bf378752fd820
jlouisugbo <jlouisugbo@gmail.com>
2025-02-17 01:46:42 -0500

    OKAY ITS DONE

--- GTmovies/settings.py
+++ GTmovies/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [ BASE_DIR / 'GTmovies/static',]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'GTmovies/static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
