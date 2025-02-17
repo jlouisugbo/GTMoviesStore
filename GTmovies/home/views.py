a25747d4f5534a317989031fed34f3d0a42abff4
jlouisugbo <jlouisugbo@gmail.com>
2025-02-16 23:39:36 -0500

    Added a frontpage scrolling movie thing

--- GTmovies/home/views.py
+++ GTmovies/home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from moviepage.models import Movie
+import random
 
 def index(request):
-    template_data = {}
-    template_data['title'] = 'Movies Store'
-    return render(request, 'home/index.html', {'template_data': template_data})
+    movies = Movie.objects.all()
+    randomized_movies = random.sample(list(movies), len(movies))  # Shuffle the list
+
+    context = {
+        'movies': randomized_movies
+    }
+    return render(request, 'home/index.html', context)
 
 def about(request):
     template_data = {}
